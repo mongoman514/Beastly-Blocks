83a111aa7faebc8e350697397e04b9543dc791cb
Tony <mongoman514@yahoo.com>
2010-12-18 19:56:32 -0500

    Semi-fixed edge of screen collision detection

--- src/actors.cpp
+++ src/actors.cpp
@@ -30,25 +30,35 @@ Tetramino::Tetramino()
 void Tetramino::handle_input()
 {
     //If a key was pressed
-    if( event.type == SDL_KEYDOWN )
+    if( event.type == SDL_MOUSEBUTTONDOWN)
     {
-        //
-        if (event.key.keysym.sym == SDLK_LEFT)
+	rotate();
+	rotation++;
+	if(rotation > 3)
 	{
-	    rotate();
-	    rotation++;
-	    if(rotation > 3)
-	    {
-		rotation = 0;
-	    }
+	    rotation = 0;
 	}
     }
-    //If a key was released
-    else if( event.type == SDL_KEYUP )
+    if(event.type == SDL_KEYDOWN)
     {
-	if (event.key.keysym.sym == SDLK_LEFT)
+	if(event.key.keysym.sym == SDLK_LEFT)
 	{
-	    
+	    xVel = -15;
+	}
+	if(event.key.keysym.sym == SDLK_RIGHT)
+	{
+	    xVel = 15;
+	}
+    }
+    if(event.type == SDL_KEYUP)
+    {
+	if(event.key.keysym.sym == SDLK_LEFT)
+	{
+	    xVel = 0;
+	}
+	if(event.key.keysym.sym == SDLK_RIGHT)
+	{
+	    xVel = 0;
 	}
     }
 }
@@ -102,26 +112,83 @@ void Tetramino::determine_form()
 
 void Tetramino::rotate()
 {
-
     if(currForm == 0)
     {
-	if(rotation == 0 || rotation == 2)
-	{
-	    blocks[1].offsets.x = blocks[0].offsets.x;
-	    blocks[1].offsets.y = blocks[0].offsets.y + 25;
-	    blocks[2].offsets.x = blocks[0].offsets.x;
-	    blocks[2].offsets.y = blocks[1].offsets.y + 25;
-	    blocks[3].offsets.x = blocks[0].offsets.x;
-	    blocks[3].offsets.y = blocks[2].offsets.y + 25;
-	}
-	if(rotation == 1 || rotation == 3)
+	if(rotation == 0) 
 	{
 	    blocks[1].offsets.x = blocks[0].offsets.x + 25;
 	    blocks[1].offsets.y = blocks[0].offsets.y;
 	    blocks[2].offsets.x = blocks[1].offsets.x + 25;
-	    blocks[2].offsets.y = blocks[1].offsets.y;
+	    blocks[2].offsets.y = blocks[0].offsets.y;
 	    blocks[3].offsets.x = blocks[2].offsets.x + 25;
-	    blocks[3].offsets.y = blocks[2].offsets.y;
+	    blocks[3].offsets.y = blocks[0].offsets.y;
+
+	    //reset width/height
+	    blocks[0].offsets.w = 25;
+	    blocks[0].offsets.h = 25;
+	    blocks[1].offsets.w = 25;
+	    blocks[1].offsets.h = 25;
+	    blocks[2].offsets.w = 25;
+	    blocks[2].offsets.h = 25;
+	    blocks[3].offsets.w = 25;
+	    blocks[3].offsets.h = 25;
+	}
+	if(rotation == 1)
+	{
+	    blocks[1].offsets.x = blocks[0].offsets.x;
+	    blocks[1].offsets.y = blocks[0].offsets.y + 25;
+	    blocks[2].offsets.x = blocks[1].offsets.x;
+	    blocks[2].offsets.y = blocks[1].offsets.y + 25;
+	    blocks[3].offsets.x = blocks[2].offsets.x;
+	    blocks[3].offsets.y = blocks[2].offsets.y + 25;
+
+	    //reset width/height
+	    blocks[0].offsets.w = 25;
+	    blocks[0].offsets.h = 25;
+	    blocks[1].offsets.w = 25;
+	    blocks[1].offsets.h = 25;
+	    blocks[2].offsets.w = 25;
+	    blocks[2].offsets.h = 25;
+	    blocks[3].offsets.w = 25;
+	    blocks[3].offsets.h = 25;
+	}
+	if(rotation == 2)
+	{
+	    blocks[1].offsets.x = blocks[0].offsets.x - 25;
+	    blocks[1].offsets.y = blocks[0].offsets.y;
+	    blocks[2].offsets.x = blocks[1].offsets.x - 25;
+	    blocks[2].offsets.y = blocks[0].offsets.y;
+	    blocks[3].offsets.x = blocks[2].offsets.x - 25;
+	    blocks[3].offsets.y = blocks[0].offsets.y;
+
+	    //reset width/height
+	    blocks[0].offsets.w = 25;
+	    blocks[0].offsets.h = 25;
+	    blocks[1].offsets.w = 25;
+	    blocks[1].offsets.h = 25;
+	    blocks[2].offsets.w = 25;
+	    blocks[2].offsets.h = 25;
+	    blocks[3].offsets.w = 25;
+	    blocks[3].offsets.h = 25;
+	}
+	if(rotation == 3)
+	{
+	    blocks[1].offsets.x = blocks[0].offsets.x;
+	    blocks[1].offsets.y = blocks[0].offsets.y - 25;
+	    blocks[2].offsets.x = blocks[1].offsets.x;
+	    blocks[2].offsets.y = blocks[1].offsets.y - 25;
+	    blocks[3].offsets.x = blocks[2].offsets.x;
+	    blocks[3].offsets.y = blocks[2].offsets.y - 25;
+
+	    //reset width/height
+	    blocks[0].offsets.w = 25;
+	    blocks[0].offsets.h = 25;
+	    blocks[1].offsets.w = 25;
+	    blocks[1].offsets.h = 25;
+	    blocks[2].offsets.w = 25;
+	    blocks[2].offsets.h = 25;
+	    blocks[3].offsets.w = 25;
+	    blocks[3].offsets.h = 25;
 	}
     }
 }
@@ -133,10 +200,133 @@ void Tetramino::gravity()
 
 void Tetramino::move()
 {
-    blocks[0].offsets.y += yVel;
-    blocks[1].offsets.y += yVel;
-    blocks[2].offsets.y += yVel; 
-    blocks[3].offsets.y += yVel;
+    //if(rotation == 0)
+    //{
+        //move block 1
+        blocks[0].offsets.y += yVel;
+        blocks[0].offsets.x += xVel;
+        if(blocks[0].offsets.y < 0 || blocks[0].offsets.y + 25 > SCREEN_HEIGHT)
+	{
+	    blocks[0].offsets.y -= yVel;
+	    blocks[1].offsets.y -= yVel;
+	    blocks[2].offsets.y -= yVel;
+	    blocks[3].offsets.y -= yVel;
+	}
+	if(blocks[0].offsets.x < 0 || blocks[0].offsets.x + 25 > SCREEN_WIDTH)
+        {
+	    blocks[0].offsets.x -= xVel;
+	    blocks[1].offsets.x -= xVel;
+	    blocks[2].offsets.x -= xVel;
+	    blocks[3].offsets.x -= xVel;
+	}
+
+        //move block 2
+        blocks[1].offsets.y += yVel;
+        blocks[1].offsets.x += xVel;
+        if(blocks[1].offsets.y < 0 || blocks[1].offsets.y + 25 > SCREEN_HEIGHT)
+	{
+	    blocks[0].offsets.y -= yVel;
+	    blocks[1].offsets.y -= yVel;
+	    blocks[2].offsets.y -= yVel;
+	    blocks[3].offsets.y -= yVel;
+	}
+	if(blocks[1].offsets.x < 0 || blocks[1].offsets.x + 25 > SCREEN_WIDTH)
+        {
+	    blocks[0].offsets.x -= xVel;
+	    blocks[1].offsets.x -= xVel;
+	    blocks[2].offsets.x -= xVel;
+	    blocks[3].offsets.x -= xVel;
+	}
+
+        //move block 3
+        blocks[2].offsets.y += yVel;
+        blocks[2].offsets.x += xVel;
+        if(blocks[2].offsets.y < 0 || blocks[2].offsets.y + 25 > SCREEN_HEIGHT)
+	{
+	    blocks[0].offsets.y -= yVel;
+	    blocks[1].offsets.y -= yVel;
+	    blocks[2].offsets.y -= yVel;
+	    blocks[3].offsets.y -= yVel;
+	}
+	if(blocks[2].offsets.x < 0 || blocks[2].offsets.x + 25 > SCREEN_WIDTH)
+        {
+	    blocks[0].offsets.x -= xVel;
+	    blocks[1].offsets.x -= xVel;
+	    blocks[2].offsets.x -= xVel;
+	    blocks[3].offsets.x -= xVel;
+	}
+
+        //move block 4
+        blocks[3].offsets.y += yVel;
+        blocks[3].offsets.x += xVel;
+	if(blocks[3].offsets.y < 0 || blocks[3].offsets.y + 25 > SCREEN_HEIGHT)
+	{
+	    blocks[0].offsets.y -= yVel;
+	    blocks[1].offsets.y -= yVel;
+	    blocks[2].offsets.y -= yVel;
+	    blocks[3].offsets.y -= yVel;
+	}
+	if(blocks[3].offsets.x < 0 || blocks[3].offsets.x + 25 > SCREEN_WIDTH)
+        {
+	    blocks[0].offsets.x -= xVel;
+	    blocks[1].offsets.x -= xVel;
+	    blocks[2].offsets.x -= xVel;
+	    blocks[3].offsets.x -= xVel;
+	}
+	
+    //}
+
+    /*the 2nd rotation
+    if(rotation == 1)
+    {
+        //move block 1
+        blocks[0].offsets.y += yVel;
+        blocks[0].offsets.x += xVel;
+        if(blocks[0].offsets.y < 0 || blocks[0].offsets.y + 25 > SCREEN_HEIGHT)
+        {
+	    blocks[0].offsets.y -= yVel;
+        }
+        if(blocks[0].offsets.x < 0 || blocks[0].offsets.x + 25 > SCREEN_WIDTH)
+        {
+	    blocks[0].offsets.x -= xVel;
+        }
+
+        //move block 2
+        blocks[1].offsets.y += yVel;
+        blocks[1].offsets.x += xVel;
+        if(blocks[1].offsets.y < 0 || blocks[1].offsets.y + 25 > SCREEN_HEIGHT)
+        {
+	    blocks[1].offsets.y -= yVel;
+        }
+        if(blocks[1].offsets.x < 0 || blocks[1].offsets.x + 25 > SCREEN_WIDTH)
+        {
+	    blocks[1].offsets.x -= xVel;
+        }
+
+        //move block 3
+        blocks[2].offsets.y += yVel;
+        blocks[2].offsets.x += xVel;
+        if(blocks[2].offsets.y < 0 || blocks[2].offsets.y + 25 > SCREEN_HEIGHT)
+        {
+	    blocks[2].offsets.y -= yVel;
+        }
+        if(blocks[2].offsets.x < 0 || blocks[2].offsets.x + 25 > SCREEN_WIDTH)
+        {
+	    blocks[2].offsets.x -= xVel;
+        }
+
+        //move block 4
+        blocks[3].offsets.y += yVel;
+        blocks[3].offsets.x += xVel;
+        if(blocks[3].offsets.y < 0 || blocks[3].offsets.y + 25 > SCREEN_HEIGHT)
+        {
+	    blocks[3].offsets.y -= yVel;
+        }
+        if(blocks[3].offsets.x < 0 || blocks[3].offsets.x + 25 > SCREEN_WIDTH)
+        {
+	    blocks[3].offsets.x -= xVel;
+        }
+    }*/
 }
 
 void Tetramino::show()
